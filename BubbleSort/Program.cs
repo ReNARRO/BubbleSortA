e696ca62ff28cadc8b8df91fc417203cc7adb07f
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2022-10-05 17:08:53 +0700

    Membuat kelas program yangdi dalamnya mendeklarasikan private interger array a yang maksimal 20. dan ada private interger array n.

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace BubbleSort
 {
-    internal class Program
+    class Program
     {
+        //Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        //Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int[] n;
+
+        //Fungsi / Method untuk menerima masukan
+        public void read()
+        {
+            //Menerima angka untuk menentukan bayaknya data yang disimpan 
+        }
         static void Main(string[] args)
         {
         }
